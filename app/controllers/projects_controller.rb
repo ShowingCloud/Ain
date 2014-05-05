05f20de38db2ebd5368e727515d0da4bff34fb78
WANG Guoqin <wangguoqin1001@gmail.com>
2014-05-05 02:24:54 -0400

    project controller: stylish and security

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,83 +1,75 @@
 class ProjectsController < ApplicationController
-  # GET /projects
-  # GET /projects.json
-  def index
-    @projects = Project.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @projects }
-    end
-  end
+	respond_to :json, :xml, :html
 
-  # GET /projects/1
-  # GET /projects/1.json
-  def show
-    @project = Project.find(params[:id])
+	before_filter :checkadmin, :except => [:create, :new]
+	before_filter :checkcaptcha, :only => [:create]
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @project }
-    end
-  end
 
-  # GET /projects/new
-  # GET /projects/new.json
-  def new
-    @project = Project.new
+	# GET /projects
+	def index
+		@projects = Project.all
+		respond_with @projects
+	end
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @project }
-    end
-  end
 
-  # GET /projects/1/edit
-  def edit
-    @project = Project.find(params[:id])
-  end
+	# GET /projects/1
+	def show
+		@project = Project.find params[:uuid]
+		respond_with @project
+	end
 
-  # POST /projects
-  # POST /projects.json
-  def create
-    @project = Project.new(params[:project])
 
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render json: @project, status: :created, location: @project }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# GET /projects/new
+	def new
+		@project = Project.new
+		respond_with @project
+	end
 
-  # PUT /projects/1
-  # PUT /projects/1.json
-  def update
-    @project = Project.find(params[:id])
 
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# GET /projects/1/edit
+	def edit
+		@project = Project.find params[:uuid]
+	end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
-  def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
 
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
-    end
-  end
+	# POST /projects
+	def create
+		@project = Project.new params[:project]
+		@project.save
+		respond_with @project
+	end
+
+
+	# PUT /projects/1
+	def update
+		@project = Project.find params[:uuid]
+		@project.update_attributes params[:project]
+		respond_with @project
+	end
+
+
+	# DELETE /projects/1
+	def destroy
+		@project = Project.find params[:uuid]
+		@project.destroy
+		respond_with @project
+	end
+
+
+	private
+
+	def checkadmin
+		if not refinery_user?
+			respond_with ret = { :status => 0 }, :location => nil, :status => :unauthorized and return
+		end
+	end
+
+
+	def checkcaptcha
+		if not simple_captcha_valid?
+			respond_with ret = { :status => 2 }, :location => nil, :status => :forbidden and return
+		end
+	end
+
 end
