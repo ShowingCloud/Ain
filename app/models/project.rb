1150f93a6b939065750aec336b839cdb7599cb25
WANG Guoqin <wangguoqin1001@gmail.com>
2014-05-19 08:39:15 -0400

    misspelling fixed

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,7 +9,7 @@ class Project < ActiveRecord::Base
 		CSV.generate(options) do |csv|
 			csv << column_names
 			all.each do |item|
-				csv << item.attributers.values_at(*column_names)
+				csv << item.attributes.values_at(*column_names)
 			end
 		end
 	end
